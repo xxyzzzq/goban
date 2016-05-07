2cc6da63ff35e2379d673774370bbe814e65b8f8
Zhiqiang Zhang <xxyzzzq@gmail.com>
2016-05-07 23:59:42 +0100

    more code

--- goban/game/client.py
+++ goban/game/client.py
@@ -4,6 +4,7 @@
 # of the MIT license.  See the LICENSE file for details.
 
 from abc import ABCMeta, abstractmethod
+from threading import Thread
 
 class Client:
     __metaclass__ = ABCMeta
@@ -11,31 +12,41 @@ class Client:
     def __init__(self, args):
         self.client_id = args['client_id']
         self.name = args['name']
+        self.__message_queue = Queue()
 
-    def connect(self, game):
+    def connect_to(self, game):
         game.connect(self)
 
-    @abstractmethod
-    def on_connected(self, args):
-        pass
+    def enqueue_message(self, message):
+        self.__message_queue.put(message)
+
+    def send_message_to_game(self, msessage):
+        self.__game.enqueue_message(message)
+
+    def __message_loop(self):
+        while True:
+            message = self.__message_queue.get()
+            if message['type'] == 'quit':
+                break
+            self._handle_message(message)
+
+    def __handle_message(self, message):
+        ''' All subclasses should call this method from parent class
+        and then handle the message themselves'''
+
+        if message['type'] == 'connected':
+            self.__on_connected(message)
+        else:
+            # Non-session message
+            self._handle_game_message(message)
+
+    def __on_connected(self, message):
+        self.__game = message['game']
+        self.__run_loop_thread = Thread(target=Client.__message_loop,
+                                        name=("Client<" + self.name + "> message loop thread"),
+                                        args=(self,))
+        self.__run_loop_thread.start()
 
     @abstractmethod
-    def prepare(self, args):
-        pass
-
-    @abstractmethod
-    def on_board_updated(self, message):
-        ''' message has the following format
-        { ["new_stone": { "coord": <coord>, ...} |
-           "changed_stone": { "coord": <coord>, ...} |
-           "removed_stone": { "coord": <coord>}] ...}
-        '''
-        pass
-
-    @abstractmethod
-    def get_next_move(self):
-        pass
-
-    @abstractmethod
-    def on_game_end(self, game_result):
+    def _handle_game_message(self, message):
         pass
--- goban/game/rule.py
+++ goban/game/rule.py
@@ -4,6 +4,7 @@
 # of the MIT license.  See the LICENSE file for details.
 
 from abc import ABCMeta, abstractmethod
+from threading import Thread
 
 class Rule:
     __metaclass__ = ABCMeta
@@ -11,17 +12,25 @@ class Rule:
     def __init__(self, game, args):
         self._game = game
         self._args = args
+        self.__message_queue = Queue()
         self._clients = {}
-        self._next_client_id = 0
 
     def connect(self, client):
-        client_id = self._next_client_id
-        self._next_client_id = self._next_client_id + 1
-        self._clients[client_id] = client
+        self._clients[client.client_id] = client
         client.on_connected(client_id)
+
+    def _send_client_message(self, client, message):
+        client.enqueue_messaeg(message)
+
+    def _broadcast_client_message(self, message):
+        for client in self._clients.values():
+            self._send_client_message(client, message)
 
     def disconnect(self, client_id):
         del self._client[client_id]
+
+    def enqueue_message(self, message):
+        self.__message_queue.put(message)
 
     @abstractmethod
     def can_connect(self, client):
@@ -35,17 +44,27 @@ class Rule:
     def _can_start(self):
         pass
 
-    def run(self, game):
-        self._run_internal(game)
+    def start_game(self, game):
+        self._game = game
+        self._setup_game()
+        self.__run_loop_thread = Thread(target=Rule.__message_loop,
+                                        name=("Rule message loop thread"),
+                                        args=(self,))
+
+    def __message_loop(self):
+        while True:
+            message = self.__message_queue.get()
+            if message['type'] == 'quit':
+                # This should only be received from Game
+                break
+            self._handle_message(message)
 
     @abstractmethod
-    def set_up(self, args):
-        ''' should assign client id, notify clients and do other initializations '''
+    def _init_game(self, game):
         pass
 
     @abstractmethod
-    def _run_internal(self, game):
-        ''' game loop '''
+    def _handle_message(self, message):
         pass
 
     @abstractmethod
